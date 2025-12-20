cceb00e65850a444385fe9bb8394db1d86aad308
bicstation <bicstation@gmail.com>
2025-12-21 00:14:24 +0900

    chore: reset git index with proper gitignore

--- django/api/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# api/utils/__init__.py
-from .common import *
--- next-tiper/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// ファイル名: ./next-tiper/globals.d.ts (または types/css.d.ts)
-
-// CSSファイルをimportする際にTypeScriptがエラーを出さないように型を宣言
-declare module '*.css' {
-  const content: string;
-  export default content;
-}
